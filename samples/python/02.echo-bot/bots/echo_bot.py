0493b19067c5eea25e693de775daed0f6bbf5f08
SPerkov <stjepan.perkovic@alkemy.com>
2024-03-05 16:36:50 +0100

    Add Azure Bot resource creds in config, and update echo_bot.py

--- samples/python/02.echo-bot/bots/echo_bot.py
+++ samples/python/02.echo-bot/bots/echo_bot.py
@@ -1,8 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+import os
+from dotenv import load_dotenv
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount
+
+
+load_dotenv()
 
 
 class EchoBot(ActivityHandler):
@@ -17,3 +22,49 @@ class EchoBot(ActivityHandler):
         return await turn_context.send_activity(
             MessageFactory.text(f"Echo: {turn_context.activity.text}")
         )
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
+from botbuilder.core.teams import TeamsInfo
+from botbuilder.schema import ChannelAccount
+import aiohttp
+import logging
+
+class EchoBot(ActivityHandler):
+
+    async def on_turn(self, turn_context: TurnContext):
+#        logging.error(f"Received activity: {turn_context.activity}")
+        
+         await super().on_turn(turn_context)
+
+    async def on_members_added_activity(
+        self, members_added: [ChannelAccount], turn_context: TurnContext
+    ):        
+        for member in members_added:
+            if member.id != turn_context.activity.recipient.id:
+                # Fetch additional information about the user
+                user_info = await TeamsInfo.get_member(turn_context, member.id)
+                user_email = user_info.email
+                user_name = user_info.name
+                logging.error(f"\n\n{user_email=}\n\n")
+                await turn_context.send_activity("Hello and welcome!")
+
+    async def on_message_activity(self, turn_context: TurnContext):
+        # Extract text from the incoming activity
+        text = turn_context.activity.text
+ 
+        async with aiohttp.ClientSession() as session:
+            url = os.getenv('GPA_API')
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {os.getenv('GPA_TOKEN')}"
+            }
+            data = {"q": text}  
+            async with session.post(url, headers=headers, json=data) as response:
+                if response.status == 200:
+                    response_data = await response.json()
+                    await turn_context.send_activity(MessageFactory.text(response_data['content']))
+                else:
+                    await turn_context.send_activity(MessageFactory.text("An error occurred while processing your request"))
+                    
--- samples/python/02.echo-bot/config.py
+++ samples/python/02.echo-bot/config.py
@@ -3,13 +3,21 @@
 # Licensed under the MIT License.
 
 import os
+from dotenv import load_dotenv
 
 """ Bot Configuration """
 
+
+load_dotenv()
 
 class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    
+    # Inter-change between these two ↓: 1. if using emulator, or 2. Azure-registered Bot resource
+
+#    APP_ID = os.environ.get("MicrosoftAppId", "")
+#    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", os.getenv('APP_ID'))
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", os.getenv('APP_PASSWORD'))
